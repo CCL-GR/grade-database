e34d1140c9b8f883de9ffa41e5eba50032c02c6f
carson <carson.linton@gmail.com>
2021-12-06 15:36:15 +0800

    mucking about

--- gradebook.lisp
+++ gradebook.lisp
@@ -9,31 +9,17 @@
     :initarg :grade-level
     :accessor grade-level)))
 
+
 ; a test student's gradebook
 (defvar *gradebook* nil)
 (defvar *staged-gradebook-entry* (make-instance 'gradebook
-                                        :name "Peter"
+                                        :name ""
                                         :set-of-grades '(((0 1) (1 5) (2 3) (3 4)) (0 1 2 3 4))
-                                        :grade-level 7))
+                                        :grade-level 0))
 
 (defmacro stage-gradebook-entry (student)
   "mutates the gradebook entry to be a specific student object representation"
   `(progn
      (setf (name *staged-gradebook-entry*) (name ,student))
-     (setf (set-of-grades *staged-gradebook-entry*) (grades ,student))
+     (setf (set-of-grades *staged-gradebook-entry*) (cons (concept-quiz ,student) (grades ,student)))
      (setf (grade-level *staged-gradebook-entry*) (grade-level ,student))))
-
-(defun add-student-to-gradebook (student)
-  "Adds a single student object to the gradebook"
-  (progn
-    (stage-gradebook-entry student)
-    (setf *gradebook* (cons *staged-gradebook-entry* *gradebook*))))
-
-(defun save-gradebook-to-db (gradebook filename)
-  "Saves gradebook to file TODO: Get this working"
-  (with-open-file (out filename
-                   :direction :output
-                   :if-exists :supersede)
-    (with-standard-io-syntax
-      (loop for stu in gradebook do
-      (print (grades stu) out)))))
--- student.lisp
+++ student.lisp
@@ -10,19 +10,24 @@
     :accessor score)
    (grades
     :initarg :grades
-    :accessor grades)))
+    :accessor grades)
+   (concept-scores
+    :initarg :concept-quiz
+    :accessor concept-scores)))
 
+(defvar *student-roster* (list "a" "b" "c" "d" "e" "f" "g" "h"))
 (defvar *staged-student* (make-instance 'student
-                                        :name "Peter"
-                                        :grade-level 7
-                                        :score 100
-                                        :grades '(((0 1) (1 5) (2 3) (3 4)) (0 1 2 3 4))))
+                                        :name ""
+                                        :grade-level 0
+                                        :score 0
+                                        :grades '(0 1 2 3 4)
+                                        :concept-scores '((0 1) (1 5) (2 5) (3 5) (4 4))))
 
-(defmacro stage-student (name grade-level score grades)
+(defmacro stage-student (name grade-level score grades concept-scores)
   "mutates the staged student to have the attributes of a given student"
  `(progn
     (setf (name *staged-student*) ,name)
     (setf (grade-level *staged-student*) ,grade-level)
     (setf (score *staged-student*) ,score)
-    (setf (grades *staged-student*) ,grades)))
-
+    (setf (grades *staged-student*) ,grades)
+    (setf (concept-scores *staged-student*) ,concept-scores)))
