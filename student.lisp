f4508129c6caa6083f559b535a162b0b478504db
carson <carson.linton@gmail.com>
2021-12-08 16:34:12 +0800

    Setf of a list into a new object

--- student.lisp
+++ student.lisp
@@ -15,7 +15,7 @@
     :initarg :concept-scores
     :accessor concept-scores)))
 
-(defvar *student-roster* (list "a" "b" "c" "d" "e" "f" "g" "h"))
+(defvar *student-roster* (list 'a 'b 'c))
 (defvar *staged-student* (make-instance 'student
                                         :name ""
                                         :grade-level 0
@@ -32,5 +32,17 @@
     (setf (grades *staged-student*) ,grades)
     (setf (concept-scores *staged-student*) ,concept-scores)))
 
-(defmacro dump-concept-scores (student)
-  `(format t "~a" (concept-scores ,student)))
+(defmacro dump-scores (student grade-set)
+  `(format t "~a" (if (equalp ,grade-set 'grades)
+                      (grades ,student)
+                      (concept-scores ,student))))
+
+(defvar *staged-student-data* (list 'student "" 7 100 '(5 5 5 5 5) '((0 5) (1 5) (2 5) (3 5) (4 5))))
+
+(defmacro instantiate-student (roster student-data)
+  "Sets the first value of the student-roster (to be expanded for loading and saving purposes)"
+  `(setf (car ,roster) (make-instance 'student :name "AAA"
+                                        :grade-level 7
+                                        :score 100
+                                        :grades '(0 1 2 3 4)
+                                        :concept-scores '((0 1) (1 5) (2 5) (3 5) (4 4)))))
