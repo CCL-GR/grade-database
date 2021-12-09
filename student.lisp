213d72ee653da9d59a5312694f9d4f2dcba73666
carson <carson.linton@gmail.com>
2021-12-09 09:23:22 +0800

    put students in roster from scrubbed data

--- student.lisp
+++ student.lisp
@@ -37,12 +37,26 @@
                       (grades ,student)
                       (concept-scores ,student))))
 
-(defvar *staged-student-data* (list 'student "" 7 100 '(5 5 5 5 5) '((0 5) (1 5) (2 5) (3 5) (4 5))))
+(defvar *staged-student-data* (list 'student "some-name" 7 100 '(5 5 5 5 5) '((0 5) (1 5) (2 5) (3 5) (4 5))))
+(defvar *staged-student-data-set* (list *staged-student-data* *staged-student-data* *staged-student-data*))
 
-(defmacro instantiate-student (roster student-data)
-  "Sets the first value of the student-roster (to be expanded for loading and saving purposes)"
-  `(setf (car ,roster) (make-instance 'student :name "AAA"
-                                        :grade-level 7
-                                        :score 100
-                                        :grades '(0 1 2 3 4)
-                                        :concept-scores '((0 1) (1 5) (2 5) (3 5) (4 4)))))
+(defmacro instantiate-student (student-data)
+  "takes a list of student data and creates a student object - list is to be SOP for creating/saving a student"
+  `(let ((name (second ,student-data))
+        (grade-level (third ,student-data))
+        (score (fourth ,student-data))
+        (grades (fifth ,student-data))
+        (concept-scores (sixth ,student-data)))
+     (make-instance 'student :name name
+                             :grade-level grade-level
+                             :score score
+                             :grades grades
+                             :concept-scores concept-scores)))
+
+(defun load-student (student student-data)
+  "Used to mapcar students into the roster"
+  (setf student (instantiate-student student-data)))
+
+(defun load-student-roster ()
+  "takes in the data for the students and puts them in the roster"
+  (setf *student-roster* (mapcar #'load-student *student-roster* *staged-student-data-set*)))
